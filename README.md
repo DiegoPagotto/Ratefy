e0be19e6987ac31081dd22d35b56c8740e3a9601
Diego Pagotto <diegodeabreupagotto@gmail.com>
2023-10-02 20:17:10 -0300

    feat (backend): add api demo with login, callback and profile endpoints

---
+++ README.md
@@ -0,0 +1,1 @@
+# ratefy-remake
---
+++ backend/api.js
@@ -0,0 +1,89 @@
+
+//#region Imports e configurações
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const querystring = require('querystring');
+const fs = require('fs');
+
+const app = express();
+const port = 3000;
+
+const configFile = 'config.json';
+const config = JSON.parse(fs.readFileSync(configFile, 'utf-8'));
+
+
+const clientId = config.clientId;
+const clientSecret = config.clientSecret;
+const redirectUri = 'http://localhost:3000/callback';
+
+app.use(cors());
+
+//#endregion
+
+app.get('/login', (req, res) => {
+    const scope = 'user-read-private user-read-email';
+    const state = 'some-random-state';
+
+    const authorizeUrl = `https://accounts.spotify.com/authorize?` +
+        `client_id=${clientId}` +
+        `&response_type=code` +
+        `&redirect_uri=${encodeURIComponent(redirectUri)}` +
+        `&scope=${encodeURIComponent(scope)}` +
+        `&state=${state}`;
+
+    res.redirect(authorizeUrl);
+});
+
+app.get('/callback', async (req, res) => {
+    const code = req.query.code; // Código de autorização retornado pelo Spotify
+
+    // Solicitar token de acesso
+    const tokenResponse = await axios({
+        method: 'post',
+        url: 'https://accounts.spotify.com/api/token',
+        data: querystring.stringify({
+            grant_type: 'authorization_code',
+            code: code,
+            redirect_uri: redirectUri,
+        }),
+        headers: {
+            'Authorization': `Basic ${Buffer.from(`${clientId}:${clientSecret}`).toString('base64')}`,
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+    });
+
+    res.redirect(`http://localhost:5173/?access_token=${tokenResponse.data.access_token}`);
+});
+
+app.get('/profile', async (req, res) => {
+    const accessTokenHeader = req.headers['authorization'];
+
+    if (!accessTokenHeader || !accessTokenHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ message: 'Token de acesso ausente ou inválido.' });
+    }
+
+    const accessToken = accessTokenHeader.split(' ')[1];
+
+    try {
+        const response = await axios.get('https://api.spotify.com/v1/me', {
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            }
+        });
+
+        const userData = response.data;
+        return res.json(userData);
+    } catch (error) {
+        console.error('Erro ao obter dados do perfil do Spotify:', error.response.data);
+        return res.status(error.response.status).json({ message: 'Erro ao obter dados do perfil do Spotify.' });
+    }
+});
+
+app.get('/ping', async (req, res) => {
+    res.send('pong');
+});
+
+app.listen(port, () => {
+    console.log(`Servidor em execução em http://localhost:${port}`);
+});
