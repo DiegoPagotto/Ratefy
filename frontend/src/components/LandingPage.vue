8ca91f5ceea9d2493d0a8f127bbd3a3e9d4dfe35
Diego Pagotto <diegodeabreupagotto@gmail.com>
2023-10-04 21:12:00 -0300

    feat (frontend): add LandingPage component and its children

---
+++ frontend/src/components/LandingPage.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="landing-page">
+        <video-background />
+        <div class="content">
+            <div class="centered">
+                <div class="vertical-center">
+                    <img src="../assets/logo.svg" alt="Logo da Ratefy" class="logo">
+                    <h1 class="text-lime">Ratefy</h1>
+                    <p>Sua opinião faz a música acontecer!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+  
+<script>
+import VideoBackground from "./VideoBackground.vue";
+
+export default {
+    components: {
+        VideoBackground,
+    },
+};
+</script>
+  
+<style scoped>
+.logo {
+    max-width: 200px;
+    margin-bottom: 20px;
+}
+
+.landing-page {
+    position: relative;
+    height: 100vh;
+}
+
+.content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.centered {
+    text-align: center;
+    color: white;
+}
+
+.vertical-center {
+    max-width: 600px;
+    margin: 0 auto;
+}
+
+.text-lime {
+    color: #1ed760;
+}
+
+.bg-lime {
+    background-color: #1ed760;
+}
+</style>
+  
---
+++ frontend/src/components/VideoBackground.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="video-background">
+        <video autoplay muted loop>
+            <source src="../assets/landing_video.mp4" type="video/mp4">
+            Seu navegador não suporta o elemento de vídeo.
+        </video>
+    </div>
+</template>
+  
+<style scoped>
+.video-background {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+}
+
+.video-background video {
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+}
+</style>
+  
