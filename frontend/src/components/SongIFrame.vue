4381d0b1e22bda0e0b9fadf6feee43b29adb0369
Diego Pagotto <diegodeabreupagotto@gmail.com>
2023-10-12 15:15:31 -0300

    feat (frontend): create SongIFrame component

---
+++ frontend/src/components/SongIFrame.vue
@@ -0,0 +1,21 @@
+<template>
+    <iframe style="border-radius:12px"
+        :src="`https://open.spotify.com/embed/track/${getSongID(songURI)}?utm_source=generator`" width="100%" height="152"
+        frameBorder="0" allowfullscreen=""
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy" class="my-3"></iframe>
+</template>
+  
+<script>
+export default {
+    props: {
+        songURI: String,
+    },
+    methods: {
+        getSongID(uri) {
+            const parts = uri.split(":");
+            return parts[parts.length - 1];
+        },
+    },
+};
+</script>
+  
