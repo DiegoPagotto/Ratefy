7c7597c78373c3e56afc5ff7a2edf29d69b949fe
Diego Pagotto <diegodeabreupagotto@gmail.com>
2023-10-11 21:29:16 -0300

    feat (frontend): add PlaylistIFrame component

---
+++ frontend/src/components/PlaylistIFrame.vue
@@ -0,0 +1,22 @@
+<template>
+    <iframe style="border-radius: 12px"
+        :src="`https://open.spotify.com/embed/playlist/${getPlaylistID(playlistURI)}?utm_source=generator`" width="100%"
+        height="600vh" frameborder="0" allowfullscreen
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy">
+    </iframe>
+</template>
+  
+<script>
+export default {
+    props: {
+        playlistURI: String,
+    },
+    methods: {
+        getPlaylistID(uri) {
+            const parts = uri.split(":");
+            return parts[parts.length - 1];
+        },
+    },
+};
+</script>
+  
