267865558206fadb4da4c97cdf380c450cd32a0d
Diego Pagotto <diegodeabreupagotto@gmail.com>
2023-10-13 00:24:18 -0300

    feat (backend): create /search endpoint

--- backend/api.js
+++ backend/api.js
@@ -147,6 +147,42 @@ app.get('/charts', async (req, res) => {
     res.send(charts);
 });
 
+app.get('/search', async (req, res) => {
+    const accessTokenHeader = req.headers['authorization'];
+
+    if (!accessTokenHeader || !accessTokenHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ message: 'Token de acesso ausente ou inválido.' });
+    }
+
+    const accessToken = accessTokenHeader.split(' ')[1];
+    const artist = {};
+    try {
+        const response = await axios.get("https://api.spotify.com/v1/search?q=" + req.query.query + "&type=artist", {
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            }
+        });
+
+        artist.info = response.data.artists.items[0];
+    } catch (error) {
+        console.log(error)
+        console.error('Erro ao obter dados do Spotify:', error.response);
+        return res.status(error.response.status).json({ message: 'Erro ao obter dados do Spotify.' });
+    }
+
+    try {
+        const response = await axios.get("https://api.spotify.com/v1/artists/"+artist.info.uri.split(':')[2]+"/top-tracks?market=BR", {
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            }
+        });
+        artist.tracks = response.data.tracks;
+    } catch (error) {
+        console.error('Erro ao obter dados do Spotify:', error.response);
+        return res.status(error.response.status).json({ message: 'Erro ao obter dados do Spotify.' });
+    }
+    res.send(artist);
+});
 
 app.get('/ping', async (req, res) => {
     res.send('pong');
