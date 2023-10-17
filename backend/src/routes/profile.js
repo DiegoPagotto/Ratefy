1b92422f16c299201f35b347b5e3b2d5d1bdd136
Diego Pagotto <diegodeabreupagotto@gmail.com>
2023-10-17 20:24:19 -0300

    refact (backend)Ç refacts profile route

--- backend/src/routes/profile.js
+++ backend/src/routes/profile.js
@@ -2,7 +2,15 @@ const express = require('express');
 const axios = require('axios');
 const profileRoute = express.Router();
 
+const User = require('../models/User');
+
 async function getUserData(accessToken) {
+    let userData = await getSpotifyData(accessToken);
+    userData = await getDatabaseData(userData);
+    return userData;
+}
+
+async function getSpotifyData(accessToken) {
     try {
         const response = await axios.get('https://api.spotify.com/v1/me', {
             headers: {
@@ -16,10 +24,34 @@ async function getUserData(accessToken) {
     }
 }
 
+async function getDatabaseData(userData) {
+    try {
+        const user = await User.findOne({
+            where: {
+                spotify_user_id: userData.id
+            },
+            attributes: ['num_resenhas', 'num_avaliacoes']
+        });
+
+        if (!user) {
+            throw new Error('Usuário não encontrado no banco de dados.');
+        }
+
+        userData.totalReviews = user.num_resenhas;
+        userData.totalRates = user.num_avaliacoes;
+
+        return userData
+
+    } catch (error) {
+        console.error('Erro ao obter dados do perfil do banco de dados:', error);
+        throw error;
+    }
+}
+
 profileRoute.get('/profile', async (req, res) => {
     const accessToken = req.accessToken;
     try {
-        const userData = await getUserData(accessToken);
+        const userData = await getUserData(accessToken)
         return res.json(userData);
     } catch (error) {
         console.error('Erro ao obter dados do perfil do Spotify:', error);
